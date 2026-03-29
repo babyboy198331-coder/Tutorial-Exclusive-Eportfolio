46833761a464d3f9643f92f6531503158ab3f78a
babyboy198331-coder <babyboy198331@gmail.com>
2026-03-28 19:22:34 -0500

    master

--- index.js
+++ index.js
@@ -2,16 +2,19 @@
 // service_ippb0pp
 // cbM7V2aKMo5f0HbPc
 
-function contact(event) {
+ function contact(event) {
     event.preventDefault();
-    emailjs
-    .sendform(
-        'service_ippb0pp',
-        'template_8dp2wge',
-        event.target,
-        'user_cbM7V2aKMo5f0HbPc'
-    ).then(() => {
-        console.log('this worked1')
-    })
+  // emailjs
+    //.sendform(
+      //  'service_ippb0pp',
+        //'template_8dp2wge',
+        //event.target,
+        //'user_cbM7V2aKMo5f0HbPc'
+    //).then(() => {
+       // console.log('this worked1')
+   // })
     
+   setTimeout(() =>{
+    console.log('it worked 1')
+   }, 500);
 }
