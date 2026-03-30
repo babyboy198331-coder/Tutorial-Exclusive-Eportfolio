8192d9f01c2ea87eec06c609a1f8ce9401ff1b59
babyboy198331-coder <babyboy198331@gmail.com>
2026-03-29 19:35:16 -0500

    master

--- index.js
+++ index.js
@@ -4,6 +4,11 @@
 
  function contact(event) {
     event.preventDefault();
+ const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+
+
+
    emailjs
     .sendform(
         'service_ippb0pp',
@@ -11,10 +16,20 @@
         event.target,
         'user_cbM7V2aKMo5f0HbPc'
  )      .then(() => {
-        console.log('this worked1')
-    },500);
+    throw new ERROR("error");
+    loading.classList.remove("modal__overlay--visible");
+    success.classList += " modal__overlay--visible";
+ }).catch(() => {
+ loading.classList.remove("modal__overlay--visible")
+ alert(
+  "the email service is temporarily unavailable. Please contact me directly on babyboy198331@gmail.com"
+ );
 
-    const loading = document.querySelector('.modal__overlay--loading');
-    const success = document.querySelector('.modal__overlay--success');
+ })
+
     loading.classList += " modal__overlay--visible"
+    setTimeout(() => {
+  
+    console.log('this worked1')
+    }, 500);
   }
