6036520f2edbcd2059f22641bb14a811a3fedbf8
babyboy198331-coder <babyboy198331@gmail.com>
2026-03-31 17:52:56 -0500

    master

--- index.js
+++ index.js
@@ -12,10 +12,9 @@ const y = event.clientY * scalefactor;
 for (let i = 0; i < shapes.length; i++) {
 const isOdd = i % 2 !== 0;
 const BoolInt = isOdd ? -1 : 1;
-shapes[i].style.transform = translate(${x  BoolInt}px, ${y  BoolInt}px);
+shapes[i].style.transform = translate(${x, BoolInt}, ${y, BoolInt});
 }
 }
-
 
 
 
